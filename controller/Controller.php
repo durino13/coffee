b5ff86302cc6e3d4335ecda84c1978171600b670
unknown <marusiju@GDMPC258.GDM.local>
2015-09-02 08:42:53 +0200

    - Backup commit ..

--- controller/Controller.php
+++ controller/Controller.php
@@ -9,61 +9,29 @@ class Controller
 	{
 		$table = new Table();
 		$squares = $table->create();
-		
+		$poolCount = 0;
+		$biggestPool = 0;
+		$poolSize = 0;
 
-		$squaresScanned = [];
-		$squaresToScan = [];
-
-		// Find the first cofee square on the table
-		$index = 357; // hardcoded ..
-
-		// Rescan all the neighbour squares next to this scanned square and store the result in squaresToScan
-		$squaresToScan = $table->getNeighbourCoffeeSquaresIndexes($index);
-		pr("Indexes found next to $index:", $squaresToScan);		
-
-		// add index of the founded square into squaresScanned
-		array_push($squaresScanned, $index);
-		pr("Squares scanned so far: ", $squaresScanned);
-		pr("Squares to scan in the next loop: ", $squaresToScan);
-
-		// Loop over the squaresToScan array and scan every square in the array
-		while (!empty($squaresToScan))
-		{
-
-			$temp = [];
-			$loop = 0;
-
-			foreach ($squaresToScan as $squareIndex) 
+		for ($i=0; $i < 399; $i++) { 
+			if ($table->getValueAtIndex($i) === 1)
 			{
-
-			 	// for every coffee square get the index of another neighbour squares 
-			 	$ns = $table->getNeighbourCoffeeSquaresIndexes($squareIndex);
-			 	pr("Squares next to $squareIndex: ", $ns);
-
-			 	// add the neighbours to scan to the list of the squarestoscan
-			 	$temp = array_unique(array_merge($temp, $ns));
-			 	pr("Squares to scan: ", $temp);
-
-				// add the scanned square into 'squaresScanned'
-				array_push($squaresScanned, $squareIndex);
-				pr('Squares scanned: ', $squaresScanned);
-
-			 	// substract the squaresScanned from the squaresToScan, so we don't scan the squares, we scanned previously
-			 	// $temp1 = array_diff($ns, $squaresScanned);
-			 	// pr('Scan this in the next loop: ', $temp1);
-
+				$extractedPool = $table->extractPoolAtIndex($i);
+				// pr('Extracted pool: ', $extractedPool);
+				// After we extract the pool out of the table, we need to clean the table on the place, from where the pool has been extracted ..
+				$table->clean($extractedPool);
+				$poolCount++;			
+				$poolSize = sizeof($extractedPool);
 			}
 
-			$squaresToScan = array_diff($temp, $squaresScanned);
-			pr('Squares to scan in the next loop: ', $squaresToScan);
-
+			if ($poolSize > $biggestPool)
+				$biggestPool = $poolSize;
 		}
-
+		
 		
 		$loader = new Twig_Loader_Filesystem(__DIR__.'/../view/');
-
 		$twig = new Twig_Environment($loader);
-		echo $twig->render('index.twig', ['squares' => $squares]);
+		echo $twig->render('index.twig', ['squares' => $squares, 'poolCount' => $poolCount, 'biggestPool' => $biggestPool]);
 
 	}
 
--- model/Table.php
+++ model/Table.php
@@ -8,7 +8,7 @@ class Table
 
 	public function __construct()
 	{
-		$this->tableSquareCount = 400;
+		$this->tableSquareCount = 399;
 		$this->init();
 	}
 
@@ -17,10 +17,10 @@ class Table
 	*/
 	private function init()
 	{
-		// for ($i=0; $i < $this->tableSquareCount; $i++) {
-		// 	$this->tableDataArray[$i] = rand(0, 1);
-		// }
-		$this->tableDataArray = [1,1,0,1,0,1,1,0,0,1,0,1,1,0,0,1,1,1,1,0,0,0,1,1,0,1,0,0,0,1,0,1,1,0,0,1,1,0,0,1,1,0,0,0,0,1,0,0,1,1,1,1,0,1,1,1,0,1,1,0,0,1,0,0,1,1,0,1,1,0,0,1,1,1,1,0,1,0,0,0,0,0,0,1,1,1,0,1,1,1,0,0,1,0,0,0,1,0,1,1,1,0,0,0,0,0,0,1,0,1,1,0,1,0,1,1,1,0,0,1,0,1,1,1,0,0,1,0,0,1,1,0,0,1,0,0,0,0,1,0,0,1,1,1,1,1,0,1,1,1,0,1,1,0,0,1,0,0,1,1,0,1,1,0,0,1,0,0,0,0,1,0,0,0,0,0,1,1,1,1,1,0,0,0,0,1,1,0,1,1,0,0,0,1,0,1,1,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,1,0,1,0,0,1,1,0,1,1,0,0,1,0,0,0,1,1,0,1,1,0,0,1,1,0,0,1,1,0,1,0,0,0,1,0,0,0,1,1,0,0,1,1,1,0,0,0,1,1,0,1,0,0,1,0,0,1,1,1,0,0,0,1,0,1,0,0,0,0,0,1,0,0,1,1,0,1,0,1,1,0,1,1,1,0,0,0,0,1,0,0,1,1,0,0,1,0,1,1,0,1,0,1,0,1,1,0,1,1,0,1,0,1,1,0,0,1,0,0,0,0,1,0,1,1,1,0,0,0,0,1,0,1,0,0,1,0,0,1,0,1,0,1,1,0,1,1,1,0,1,0,0,0,1,0,0,0,1,0,0,0,1,1,0,1,1,1,0,0,1,0,1,1,1,0,1,1,1,1,1,1];
+		for ($i=0; $i <= $this->tableSquareCount; $i++) {
+			$this->tableDataArray[$i] = rand(0, 1);
+		}
+		// $this->tableDataArray = [1,1,0,1,0,1,1,0,0,1,0,1,1,0,0,1,1,1,1,0,0,0,1,1,0,1,0,0,0,1,0,1,1,0,0,1,1,0,0,1,1,0,0,0,0,1,0,0,1,1,1,1,0,1,1,1,0,1,1,0,0,1,0,0,1,1,0,1,1,0,0,1,1,1,1,0,1,0,0,0,0,0,0,1,1,1,0,1,1,1,0,0,1,0,0,0,1,0,1,1,1,0,0,0,0,0,0,1,0,1,1,0,1,0,1,1,1,0,0,1,0,1,1,1,0,0,1,0,0,1,1,0,0,1,0,0,0,0,1,0,0,1,1,1,1,1,0,1,1,1,0,1,1,0,0,1,0,0,1,1,0,1,1,0,0,1,0,0,0,0,1,0,0,0,0,0,1,1,1,1,1,0,0,0,0,1,1,0,1,1,0,0,0,1,0,1,1,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,1,0,1,0,0,1,1,0,1,1,0,0,1,0,0,0,1,1,0,1,1,0,0,1,1,0,0,1,1,0,1,0,0,0,1,0,0,0,1,1,0,0,1,1,1,0,0,0,1,1,0,1,0,0,1,0,0,1,1,1,0,0,0,1,0,1,0,0,0,0,0,1,0,0,1,1,0,1,0,1,1,0,1,1,1,0,0,0,0,1,0,0,1,1,0,0,1,0,1,1,0,1,0,1,0,1,1,0,1,1,0,1,0,1,1,0,0,1,0,0,0,0,1,0,1,1,1,0,0,0,0,1,0,1,0,0,1,0,0,1,0,1,0,1,1,0,1,1,1,0,1,0,0,0,1,0,0,0,1,0,0,0,1,1,0,1,1,1,0,0,1,0,1,1,1,0,1,1,1,1,1,1];
 	}	
 
 	/**
@@ -31,27 +31,112 @@ class Table
 		return $this->tableDataArray;
 	}
 
+	/**
+	* Clean the table 
+	*/
+	public function clean($pool)
+	{
+		foreach ($pool as $poolIndex) {
+			$this->tableDataArray[$poolIndex] = 0;
+		}
+	}
+
+	public function extractPoolAtIndex($index)
+	{
+		$squaresScanned = [];
+		$squaresToScan = [];
+
+		// Rescan all the neighbour squares next to this scanned square and store the result in squaresToScan
+		$squaresToScan = $this->getNeighbourCoffeeSquaresIndexes($index);
+		// pr("Indexes found next to $index:", $squaresToScan);		
+
+		// add index of the founded square into squaresScanned
+		array_push($squaresScanned, $index);
+		// pr("Squares scanned so far: ", $squaresScanned);
+		// pr("Squares to scan in the next loop: ", $squaresToScan);
+
+		// Loop over the squaresToScan array and scan every square in the array
+		while (!empty($squaresToScan))
+		{
+
+			$temp = [];
+			$loop = 0;
+
+			foreach ($squaresToScan as $squareIndex) 
+			{
+
+			 	// for every coffee square get the index of another neighbour squares 
+			 	$ns = $this->getNeighbourCoffeeSquaresIndexes($squareIndex);
+			 	// pr("Squares next to $squareIndex: ", $ns);
+
+			 	// add the neighbours to scan to the list of the squarestoscan
+			 	$temp = array_unique(array_merge($temp, $ns));
+			 	// pr("Squares to scan: ", $temp);
+
+				// add the scanned square into 'squaresScanned'
+				array_push($squaresScanned, $squareIndex);
+				// pr('Squares scanned: ', $squaresScanned);
+
+			}
+
+			$squaresToScan = array_diff($temp, $squaresScanned);
+			// pr('Squares to scan in the next loop: ', $squaresToScan);
+
+		}		
+
+		return $squaresScanned;
+	}
+
 	/** 
 	* This function will count coffee squares next to a square with a specific index
 	*/
 	public function getNeighbourCoffeeSquaresIndexes($index)	
 	{
-		$offsets = [-21, -20, -19, -1, 1, 19, 20, 21];
-
+		$offsets = $this->getOffsets($index);
 		$indexesFound = [];
 
 		foreach ($offsets as $offset) {
-			if (($index-$offset >= 0) && ($index+$offset <= $this->tableSquareCount))
+
+			if (array_key_exists($index+$offset, $this->tableDataArray))
 			{
-				if ($this->tableDataArray[$index-$offset] === 1)
+				if ($this->tableDataArray[$index+$offset] === 1)
 				{
-					array_push($indexesFound, $index-$offset);
-				}					
-			}
+					array_push($indexesFound, $index+$offset);
+				}						
+			}			
 						
 		}
 
 		return $indexesFound;
 	}
+
+	/** 
+	* Function will get a position 
+	*/
+	private function getOffsets($index)
+	{
+		$leftTableIndexes = [0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300, 320, 340, 360, 380, 400];
+		$rightTableIndexes = [19, 39, 59, 79, 99, 119, 139, 159, 179, 199, 219, 239, 259, 279, 299, 319, 339, 359, 379, 399];
+
+		$allOffsets = [-21, -20, -19, -1, 1, 19, 20, 21];
+		$offsetsForLeft = [-20, -19, 1, 20, 21];
+		$offsetsForRight = [-21, -20, -1, 1, 20];		
+
+		if (in_array($index, $leftTableIndexes))
+		{
+			return $offsetsForLeft;
+		} else if (in_array($index, $rightTableIndexes))
+		{
+			return $offsetsForRight;
+		} else {
+			return $allOffsets;
+		}
+
+	}
+
+	public function getValueAtIndex($index)
+	{
+		return $this->tableDataArray[$index];
+	}	
 
 }
