f52f2e2a5010c6635ff1b63e1e1b47ea927240d6
unknown <marusiju@GDMPC258.GDM.local>
2015-09-07 14:34:34 +0200

    - Added more colors .. - Coffee squares are generated with lower probabbility .. Now it's not 50-50, but 60-40 .. It looks better + there are more pools created ..

--- model/Table.php
+++ model/Table.php
@@ -11,6 +11,32 @@ class Table
         $this->tableSquareCount = 399;
         $this->pourCoffee();
     }
+    
+      /**
+     * getRandomWeightedElement()
+     * Utility function for getting random values with weighting.
+     * Pass in an associative array, such as array('A'=>5, 'B'=>45, 'C'=>50)
+     * An array like this means that "A" has a 5% chance of being selected, "B" 45%, and "C" 50%.
+     * The return value is the array key, A, B, or C in this case.  Note that the values assigned
+     * do not have to be percentages.  The values are simply relative to each other.  If one value
+     * weight was 2, and the other weight of 1, the value with the weight of 2 has about a 66%
+     * chance of being selected.  Also note that weights should be integers.
+     * 
+     * @param array $weightedValues
+     */
+    private function getRandomWeightedElement(array $weightedValues)
+    {
+        $rand = mt_rand(1, (int) array_sum($weightedValues));
+
+        foreach ($weightedValues as $key => $value)
+        {
+            $rand -= $value;
+            if ($rand <= 0)
+            {
+                return $key;
+            }
+        }
+    }
 
     /**
      * Initialize the table data with random values
@@ -22,8 +48,7 @@ class Table
         
         for ($i = 0; $i < 400; $i++)
         {
-//            $this->squaresArray[$i] = new TableSquare($i, $testValues[$i]);
-             $this->squaresArray[$i] = new TableSquare($i, rand(0, 1));
+             $this->squaresArray[$i] = new TableSquare($i, $this->getRandomWeightedElement(array(0=>60, 1=>40)));
         }
     }
 
